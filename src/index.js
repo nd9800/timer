950720e32babf1c28f85831613e04d395d1d4f11
NamD <64962991+nd9800@users.noreply.github.com>
2021-08-10 13:47:42 +0700

    8/10

--- src/index.js
+++ src/index.js
@@ -1,15 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+class Timer extends React.Component {
+    state = {
+        min : 0,
+        sec : 0
+    }
+    setTime = () =>
+    {
+        this.setState({
+            min : document.querySelector('.startTime').value,
+            sec : 0
+        })
+
+    };
+    updateSec()
+    {
+        if(this.state.sec === 0)
+        {
+            this.setState({
+                sec: 60
+            })
+            if(this.state.min <= 0 )
+            {
+                this.setState({
+                    min: 0
+                })
+            }
+            else {
+                this.setState({
+                    min: this.state.min - 1,
+                });
+            }
+        }
+            this.setState({
+                sec: this.state.sec - 1
+            })
+    };
+
+
+
+
+    startTime = () =>
+    {
+      let clear =  setInterval(()=>{
+            this.updateSec();
+            console.log(this.state.sec);
+      if(this.state.min ===0 && this.state.sec ===0)
+      {
+          clearInterval(clear);
+      }
+        },100);
+
+
+    }
+
+    render() {
+
+            return  <div className="calButton">
+                <h1>TIMER</h1>
+                <input className='startTime'/>
+                <button onClick={this.setTime}>Choose</button>
+
+                <ul>
+                    <li><strong>{this.state.min}</strong> min </li>
+                    <li><strong>{this.state.sec}</strong> sec </li>
+                </ul>
+                <button onClick={this.startTime}>START</button>
+
+            </div>;
+    }
+}
+
+const el = <Timer />;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    el,
+    document.getElementById("container")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
